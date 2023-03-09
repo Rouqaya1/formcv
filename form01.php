02c6b0932ac0d0ceca1402674f1cadeebefccd53
Rouqaya1 <126081002+Rouqaya1@users.noreply.github.com>
2023-03-09 13:56:46 +0100

    Initial commit

---
+++ form01.php
@@ -0,0 +1,28 @@
+<?php
+
+	$Civilite = htmlspecialchars($_POST["Civilite"]);
+	$Nom = htmlspecialchars($_POST["Nom"]);
+	$Prenom = htmlspecialchars($_POST["Prenom"]);
+	$Remarques = htmlspecialchars($_POST["Remarques"]);
+	$Mail = htmlspecialchars($_POST["Mail"]);
+	$Telephone = htmlspecialchars($_POST["Telephone"]);
+	$Societe = htmlspecialchars($_POST["Societe"]);
+echo("ça fonctionne enfin ");
+
+	// 1 : on ouvre le fichier
+	$monfichier = fopen('donneesformulaire.txt', 'a+');
+	fputs($monfichier, "==================================\n"); 
+	fputs($monfichier, $Nom."\n"); 
+	fputs($monfichier, $Prenom."\n"); 
+	fputs($monfichier, $Remarques."\n"); 
+	fputs($monfichier, $Mail."\n");
+	fputs($monfichier, $Telephone."\n");
+	fputs($monfichier, $Civilite."\n");
+
+	// 3 : quand on a fini de l'utiliser, on ferme le fichier
+	fclose($monfichier);
+
+?>
+
+
+
