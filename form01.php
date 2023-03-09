6d593cfa6a1036301b1923a4a49a074c71c552d1
Rouqaya1 <126081002+Rouqaya1@users.noreply.github.com>
2023-03-09 15:22:04 +0100

    Finish

--- form01.php
+++ form01.php
@@ -12,12 +12,13 @@ echo("ça fonctionne enfin ");
 	// 1 : on ouvre le fichier
 	$monfichier = fopen('donneesformulaire.txt', 'a+');
 	fputs($monfichier, "==================================\n"); 
+	fputs($monfichier, $Civilite."\n");
 	fputs($monfichier, $Nom."\n"); 
-	fputs($monfichier, $Prenom."\n"); 
-	fputs($monfichier, $Remarques."\n"); 
+	fputs($monfichier, $Prenom."\n");
+	fputs($monfichier, $Societe."\n"); 
 	fputs($monfichier, $Mail."\n");
 	fputs($monfichier, $Telephone."\n");
-	fputs($monfichier, $Civilite."\n");
+	fputs($monfichier, $Remarques."\n");
 
 	// 3 : quand on a fini de l'utiliser, on ferme le fichier
 	fclose($monfichier);
